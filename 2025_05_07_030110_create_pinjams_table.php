d89d4d6dda75710355ee00575d48e1299d1dce22
Syawal Ichwanul Rafki <syawalichwanulr@gmail.com>
2026-02-13 00:51:21 +0700

    Add files via upload

---
+++ 2025_05_07_030110_create_pinjams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pinjams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id')->constrained('mahasiswas')->onDelete('cascade');         
+            $table->foreignId('buku_id')->constrained('bukus')->onDelete('cascade'); 
+            $table->date('tanggal_pinjam'); 
+            $table->date('tanggal_kembali')->nullable(); 
+            $table->enum('status', ['dipinjam', 'dikembalikan'])->default('dipinjam');         
+            $table->timestamps();  
+    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pinjams');
+    }
+};
